b97bb5df0ea1ca39c2aa031b942ebe663fb95886
Hardik Modha <hardik.modha@goniyo.com>
2022-03-14 11:10:29 +0530

    Squashed commit of the following:
    commit c277bdfa1ebdb273623c75d2198062e5c6a9ef3b
    Author: Hardik Modha <hardik.modha@goniyo.com>
    Date:   Fri Mar 11 16:37:05 2022 +0530
    
        Added Modal and demo cell folders
    
    commit 1ba63fa8f8af42bfa4c258b9360e252527cb7465
    Author: Hardik.ItPath <100897512+hardikitpath@users.noreply.github.com>
    Date:   Fri Mar 4 12:39:16 2022 +0530
    
        Update AppConstant.swift
    
        Remove Appcosntans file
    
    commit f634a8d46e1743ca38c3811acba4ae22cd76a3ae
    Author: Hardik.ItPath <100897512+hardikitpath@users.noreply.github.com>
    Date:   Fri Mar 4 11:56:13 2022 +0530
    
        Update UIImageView+Loading.swift
    
        Added Alamofire Image library
    
    commit 424b6752466de190f466eca087a661f14c0de8ed
    Merge: f45904c 47ac73b
    Author: Hardik.ItPath <100897512+hardikitpath@users.noreply.github.com>
    Date:   Fri Mar 4 11:32:06 2022 +0530
    
        Merge pull request #1 from hardikitpath/feature/Improvment
    
        Added Appconstant file

--- ProjectStructureSample/Utility/AppConstant.swift
+++ ProjectStructureSample/Utility/AppConstant.swift
@@ -9,7 +9,22 @@ import Foundation
 
 struct AppConstant {
     
+    //MARK: - ValidationMessage
     enum ValidationMessage {
         static let invialdEmail = "Email address is not valid"
+    }
+    
+    //MARK: - BaseURLs
+    enum BaseURLs {
+        static let liveBaseURLs = ""
+        static let stagingBaseURLs = ""
+        static let localBaseURLs = ""
+    }
+    
+    //MARK: - ButtonTitles
+    enum ButtonTitles {
+        static let ok = "Ok"
+        static let cancel = "Cancel"
+        static let confirm = "Confirm"
     }
 }
